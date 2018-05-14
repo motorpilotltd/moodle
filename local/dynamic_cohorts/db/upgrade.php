5023f2e9cd8808ac0a63d61c240f6380d42fd27c
Simon Lewis <simon-m.lewis@arup.com>
2018-05-14 15:50:43 +0100

    Fixes to cohorts and certifications tasks.

--- local/dynamic_cohorts/db/upgrade.php
+++ local/dynamic_cohorts/db/upgrade.php
@@ -13,21 +13,20 @@ function xmldb_local_dynamic_cohorts_upgrade($oldversion) {
         }
     }
 
-    if ($oldversion < 2018040602) {
-        $syscontextid = \context_system::instance()->id;
+    if ($oldversion < 2018040604) {
+        // Update existing records.
+        $sql = 'UPDATE {wa_cohort_roles} SET contextid = :syscontextid WHERE contextid IS NULL';
+        $DB->execute($sql, ['syscontextid' => SYSCONTEXTID]);
+
         // Increase size of field and make not null to match install file. Also update default.
         $table = new xmldb_table('wa_cohort_roles');
-        $field = new xmldb_field('contextid', XMLDB_TYPE_INTEGER, 10, null, XMLDB_NOTNULL, null, $syscontextid);
+        $field = new xmldb_field('contextid', XMLDB_TYPE_INTEGER, 10, null, XMLDB_NOTNULL, null, SYSCONTEXTID);
 
         if ($dbman->field_exists($table, $field)) {
             // Will do the size and NOT NULL settings, other calls are just wrappers for this.
             $dbman->change_field_type($table, $field);
             $dbman->change_field_default($table, $field);
         }
-
-        // Update existing records.
-        $sql = 'UPDATE {wa_cohort_roles} SET contextid = :syscontextid WHERE contextid IS NULL';
-        $DB->execute($sql, ['syscontextid' => $syscontextid]);
     }
 
     return true;
--- local/dynamic_cohorts/version.php
+++ local/dynamic_cohorts/version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018040602; //v1.11.1 (Arup bugfix).
+$plugin->version = 2018040604; //v1.11.1 (Arup bugfix).
 $plugin->requires = 2015051104;
 $plugin->component = 'local_dynamic_cohorts';
