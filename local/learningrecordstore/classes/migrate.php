aebfe241a46e4e2570ce5ebeea5767c74a79f698
Andrew Hancox <andrewdchancox@googlemail.com>
2018-12-20 11:02:49 +0000

    local_learningrecordstore: Started migration script

---
+++ local/learningrecordstore/classes/migrate.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrewhancox
+ * Date: 12/07/2018
+ * Time: 10:59
+ */
+
+namespace local_learningrecordstore;
+
+class migrate {
+    public static function deprecate_coursemanager() {
+        global $DB;
+
+        $events = [
+                '\local_coursemanager\event\class_created' => '\mod_tapsenrol\event\class_created',
+                '\local_coursemanager\event\class_updated' => '\mod_tapsenrol\event\class_updated'
+        ];
+
+        foreach ($events as $from => $to) {
+            $DB->execute("update {logstore_standard_log} set eventname = :to where eventname = :from",
+                    ['from' => $from, 'to' => $to]
+            );
+        }
+    }
+}
