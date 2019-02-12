e6c228254fe5cfe3fa7a3544cc2e6d08030f6efb
Simon Lewis <simon-m.lewis@arup.com>
2019-02-12 09:32:58 +0000

    local_custom_certification - #58397 - Task updates ready for Ethics.

--- local/custom_certification/settings.php
+++ local/custom_certification/settings.php
@@ -3,8 +3,16 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($hassiteconfig) {
-    $settings = new admin_externalpage('local_custom_certification', get_string('pluginname', 'local_custom_certification'), "$CFG->wwwroot/local/custom_certification/index.php", 'local/custom_certification:view');
+    $ADMIN->add('root', new admin_category('local_custom_certification', get_string('pluginname', 'local_custom_certification')));
 
-    $ADMIN->add('root', $settings);
+    $adminpage = new admin_externalpage('local_custom_certification_admin', get_string('adminpage', 'local_custom_certification'), "$CFG->wwwroot/local/custom_certification/index.php", 'local/custom_certification:view');
+    $ADMIN->add('local_custom_certification', $adminpage);
+
+    $settings = new admin_settingpage('local_custom_certification_settings', get_string('settings', 'local_custom_certification'));
+    $ADMIN->add('local_custom_certification', $settings);
+
+    $name = 'local_custom_certification/send_message_rate';
+    $title = get_string('setting:send_message_rate', 'local_custom_certification');
+    $description = get_string('setting:send_message_rate_desc', 'local_custom_certification');
+    $settings->add(new admin_setting_configtext($name, $title, $description, 100, PARAM_INT));
 }
-
