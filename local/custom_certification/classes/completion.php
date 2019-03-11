e90fe5d93a1b2acd64453cbef68ede6892974ac7
Simon Lewis <simon-m.lewis@arup.com>
2019-03-11 14:13:04 +0000

    local_custom_certification - #59462 - Certification completion check, on course completion, update.

--- local/custom_certification/classes/completion.php
+++ local/custom_certification/classes/completion.php
@@ -390,7 +390,7 @@ class completion
     public static function update_completion_status($certification, $userid, $certifpath, $status){
         global $DB;
         $firecompletionevent = false;
-        if(!is_object($certification)){
+        if (!is_object($certification)) {
             $certification = new certification($certification, false);
         }
         // Check if record exists or need to be created.
@@ -403,18 +403,40 @@ class completion
 
         // Pre-load ready to override if completed in TAPS.
         $progress = self::get_user_progress($certification, $userid);
+        $tapsenrolmentid = null;
+        $timecompleted = null;
 
-        // Are we linked to a old TAPS course? And this user already been linked to an enrolment?
-        list($tapsenrolmentid, $timecompleted) = self::check_taps_completion($certification, $userid, $completionrecord);
-        // Override status if applicable (TAPS completion found).
-        if (!is_null($timecompleted)) {
-            // Override status and progress.
-            $status = self::COMPLETION_STATUS_COMPLETED;
-            $progress['certification'] = 100;
-            $progress['recertification'] = 100;
+        // Handle a current complete certification vs not complete/non-existent..
+        if ($completionrecord
+                && $completionrecord->status == self::COMPLETION_STATUS_COMPLETED
+                && $status == self::COMPLETION_STATUS_COMPLETED) {
+            // Record exists, is already complete, and passed status is complete.
+            $newcompletiontime = self::get_completion_time($certification, $userid, (int) $certification->has_recertification());
+            if ($newcompletiontime > $completionrecord->timecompleted) {
+                // We have a newer completion, archive existing record and clear down.
+                $insertrecord = clone $completionrecord;
+                $insertrecord->id = null;
+                $insertrecord->timearchived = time();
+                if ($DB->insert_record('certif_completions_archive', $insertrecord)) {
+                    $DB->delete_records('certif_completions', ['id' => $completionrecord->id]);
+                    self::delete_courseset_completion_data($completionrecord->certifid, $completionrecord->userid);
+                    // Re-run completion check.
+                    self::check_completion($completionrecord->certifid, $completionrecord->userid, (int) $certification->has_recertification());
+                }
+            }
+        } else {
+            // Are we linked to a old TAPS course? And this user already been linked to an enrolment?
+            list($tapsenrolmentid, $timecompleted) = self::check_taps_completion($certification, $userid, $completionrecord);
+            // Override status if applicable (TAPS completion found).
+            if (!is_null($timecompleted)) {
+                // Override status and progress.
+                $status = self::COMPLETION_STATUS_COMPLETED;
+                $progress['certification'] = 100;
+                $progress['recertification'] = 100;
+            }
         }
 
-        if(!$completionrecord){
+        if (!$completionrecord) {
             $completionrecord = new \stdClass();
             $completionrecord->userid = $userid;
             $completionrecord->certifid = $certification->id;
@@ -424,25 +446,25 @@ class completion
             $completionrecord->cronchecked = 1;
             $completionrecord->tapsenrolmentid = $tapsenrolmentid;
 
-            if($status == self::COMPLETION_STATUS_COMPLETED){
+            if ($status == self::COMPLETION_STATUS_COMPLETED) {
                 $firecompletionevent = true;
                 $completionrecord->timecompleted =
                         (is_null($timecompleted) ? self::get_completion_time($certification, $userid, $certifpath) : $timecompleted);
                 /**
                  * If certification has recertification path then calculate time expire and window open time
                  */
-                if($certification->has_recertification()) {
+                if ($certification->has_recertification()) {
                     $completionrecord->timeexpires = self::get_expiration_time($certification, $userid, $completionrecord->timecompleted);
                     $datetime = new \DateTime();
                     $datetime->setTimestamp($completionrecord->timeexpires);
                     $interval = new \DateInterval('P'.$certification->windowperiod.certification::get_time_period_for_interval($certification->windowperiodunit));
                     $datetime->sub($interval);
                     $completionrecord->timewindowsopens = $datetime->getTimestamp();
-                }else{
+                } else {
                     $completionrecord->timeexpires = 0;
                     $completionrecord->timewindowsopens = 0;
                 }
-            }else{
+            } else {
                 $completionrecord->timecompleted = null;
                 $completionrecord->timeexpires = null;
                 $completionrecord->timewindowsopens = null;
@@ -453,28 +475,30 @@ class completion
             $completionrecord->timemodified = time();
             $completionrecord->id = $DB->insert_record('certif_completions', $completionrecord, true);
         } else {
-            if($status == self::COMPLETION_STATUS_COMPLETED && $completionrecord->status != self::COMPLETION_STATUS_COMPLETED){
+            // Record already exists.
+            if ($status == self::COMPLETION_STATUS_COMPLETED && $completionrecord->status != self::COMPLETION_STATUS_COMPLETED) {
                 $completionrecord->tapsenrolmentid =
                         (is_null($completionrecord->tapsenrolmentid) ? $tapsenrolmentid : $completionrecord->tapsenrolmentid);
                 $completionrecord->timecompleted =
                         (is_null($timecompleted) ? self::get_completion_time($certification, $userid, $certifpath) : $timecompleted);
-                /**
-                 * If certification has recertification path then calculate time expire and window open time
-                 */
-                if($certification->has_recertification()){
+
+                // If certification has recertification path then calculate time expire and window open time.
+                if ($certification->has_recertification()) {
                     $completionrecord->timeexpires = self::get_expiration_time($certification, $userid, $completionrecord->timecompleted);
                     $datetime = new \DateTime();
                     $datetime->setTimestamp($completionrecord->timeexpires);
                     $interval = new \DateInterval('P'.$certification->windowperiod.certification::get_time_period_for_interval($certification->windowperiodunit));
                     $datetime->sub($interval);
                     $completionrecord->timewindowsopens = $datetime->getTimestamp();
-                }else{
+                } else {
                     $completionrecord->timeexpires = 0;
                     $completionrecord->timewindowsopens = 0;
                 }
 
                 $firecompletionevent = true;
             }
+
+            // This happens whether complete or not, to update progress.
             $completionrecord->cronchecked = 1;
             $completionrecord->progress = $certifpath == certification::CERTIFICATIONPATH_BASIC ? $progress['certification'] : $progress['recertification'];
             $completionrecord->status = $status;
@@ -489,7 +513,7 @@ class completion
         /**
          * Trigger certification_completed event if status is changed to completed
          */
-        if($firecompletionevent){
+        if ($firecompletionevent) {
             $context = \context_system::instance();
             $params = [
                 'context' => $context,
