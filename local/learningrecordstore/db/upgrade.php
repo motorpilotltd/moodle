32d8a98b297ede60a5df596f9add484561171165
Andrew Hancox <andrewdchancox@googlemail.com>
2019-06-07 11:26:51 +0100

    local_learningrecordstore: Add upgrade script

--- local/learningrecordstore/db/upgrade.php
+++ local/learningrecordstore/db/upgrade.php
@@ -33,7 +33,48 @@ function xmldb_local_learningrecordstore_upgrade($oldversion) {
 
     $dbman = $DB->get_manager();
 
-    if ($oldversion < 2015111608) {
+
+    if ($oldversion < 2015111610) {
+        // Define table local_admin_user_update_log to be created.
+        $table = new xmldb_table('local_learningrecordstore');
+
+        // Adding fields to table local_admin_user_update_log
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('provider', XMLDB_TYPE_TEXT);
+        $table->add_field('healthandsafetycategory', XMLDB_TYPE_TEXT);
+        $table->add_field('providerid', XMLDB_TYPE_INTEGER, '10');
+        $table->add_field('providername', XMLDB_TYPE_TEXT);
+        $table->add_field('staffid', XMLDB_TYPE_INTEGER, '10');
+        $table->add_field('duration', XMLDB_TYPE_FLOAT, '10,5');
+        $table->add_field('durationunits', XMLDB_TYPE_TEXT);
+        $table->add_field('completiontime', XMLDB_TYPE_INTEGER, '10');
+        $table->add_field('description', XMLDB_TYPE_TEXT);
+        $table->add_field('certificateno', XMLDB_TYPE_TEXT);
+        $table->add_field('classcategory', XMLDB_TYPE_TEXT);
+        $table->add_field('classcost', XMLDB_TYPE_NUMBER, '20,5', null, null, null, null);
+        $table->add_field('classcostcurrency', XMLDB_TYPE_TEXT);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10');
+        $table->add_field('expirydate', XMLDB_TYPE_INTEGER, '10');
+        $table->add_field('classtype', XMLDB_TYPE_TEXT);
+        $table->add_field('archived', XMLDB_TYPE_INTEGER, '4');
+        $table->add_field('locked', XMLDB_TYPE_INTEGER, '4');
+        $table->add_field('location', XMLDB_TYPE_TEXT);
+
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Adding indexes to table local_admin_user_update_log.
+        $table->add_index('staffid', XMLDB_INDEX_NOTUNIQUE, array('staffid'));
+
+        // Conditionally launch create table for local_admin_user_update_log
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Savepoint reached.
+        upgrade_plugin_savepoint(true, 2015111610, 'local', 'learningrecordstore');
+    }
+
+    if ($oldversion < 2015111611) {
         $table = new xmldb_table('local_learningrecordstore');
 
         $field = new xmldb_field('starttime', XMLDB_TYPE_INTEGER, '10', null, false, null, null);
@@ -42,7 +83,7 @@ function xmldb_local_learningrecordstore_upgrade($oldversion) {
         $field = new xmldb_field('endtime', XMLDB_TYPE_INTEGER, '10', null, false, null, null);
         $dbman->change_field_notnull($table, $field);
 
-        upgrade_plugin_savepoint(true, 2015111608, 'local', 'learningrecordstore');
+        upgrade_plugin_savepoint(true, 2015111611, 'local', 'learningrecordstore');
     }
 
     return true;
