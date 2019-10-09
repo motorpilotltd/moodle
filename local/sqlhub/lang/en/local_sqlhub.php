3f2c6d503d23f29f47ca340ca2f1240a1e4d4050
Andrew Hancox <andrewdchancox@googlemail.com>
2019-10-09 11:45:54 +0100

    local_sqlhub: Manage indexes on hub table

---
+++ local/sqlhub/lang/en/local_sqlhub.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrewhancox
+ * Date: 24/07/2019
+ * Time: 11:36
+ */
+
+
+$string['checktablestatus'] = 'Check sql hub table status';
+$string['pluginname'] = 'Check sql hub table status';
