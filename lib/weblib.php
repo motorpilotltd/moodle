0093602d1bc8d2b390ca5e68bfff8a9699220f7a
Simon Lewis <simon-m.lewis@arup.com>
2019-10-22 16:22:48 +0100

    core - lib/weblib - #66800 - Update email validation to defer to phpmailer

--- lib/weblib.php
+++ lib/weblib.php
@@ -1087,13 +1087,12 @@ function page_get_doc_link_path(moodle_page $page) {
  * @return boolean
  */
 function validate_email($address) {
+/* BEGIN CORE MOD - BACKPORTED */
+    global $CFG;
+    require_once($CFG->libdir.'/phpmailer/moodle_phpmailer.php');
 
-    return (bool)preg_match('#^[-!\#$%&\'*+\\/0-9=?A-Z^_`a-z{|}~]+'.
-                 '(\.[-!\#$%&\'*+\\/0-9=?A-Z^_`a-z{|}~]+)*'.
-                  '@'.
-                  '[-!\#$%&\'*+\\/0-9=?A-Z^_`a-z{|}~]+\.'.
-                  '[-!\#$%&\'*+\\./0-9=?A-Z^_`a-z{|}~]+$#',
-                  $address);
+    return moodle_phpmailer::validateAddress($address);
+/* END CORE MOD - BACKPORTED */
 }
 
 /**
