1544a07dde2577f38b2e9fd66270d6d1c2fe25b5
Simon Lewis <simon-m.lewis@arup.com>
2020-04-22 17:01:21 +0100

    local_search - #74864 - LiL placeholder in seearch.

--- local/search/renderer.php
+++ local/search/renderer.php
@@ -14,15 +14,19 @@ class local_search_renderer extends plugin_renderer_base
         return $this->render_from_template('local_search/searchform', $formdata);
     }
 
-    public function search_results($resultdata)
+    public function search_results($resultdata, $search)
     {
-        $resultdata['preloadedcourses'] = $this->courses($resultdata);
+        $resultdata['preloadedcourses'] = $this->courses($resultdata, $search);
 
         return $this->render_from_template('local_search/searchresults', $resultdata);
     }
 
-    public function courses($resultdata)
+    public function courses($resultdata, $search)
     {
+        global $OUTPUT;
+        $resultdata['lilimgurl'] = $OUTPUT->image_url('lil-logo', 'local_search');
+        $lilsearchurl = new moodle_url('https://www.linkedin.com/learning/search', ['keywords' => $search]);
+        $resultdata['lilsearchurl'] = $lilsearchurl->out(false);
         return $this->render_from_template('local_search/courses', $resultdata);
     }
 }
