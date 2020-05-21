cb05865018c31301b3b312197a1829c5a7d07221
Andrew Hancox <andrewdchancox@googlemail.com>
2020-05-21 16:34:35 +0100

    rbsource_courseracourses: Build

---
+++ local/reportbuilder/sources/courseracourses/lang/en/rbsource_courseracourses.php
@@ -0,0 +1,8 @@
+<?php
+
+// Report source title.
+$string['sourcetitle'] = 'Coursera Course';
+
+$string['title'] = 'Title';
+$string['estimatedlearningtime'] = 'Estimated learning time';
+$string['type_coursera'] = 'Coursera Course';
