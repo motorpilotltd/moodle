69d7b349dd7016f6521bd6b435976b1446aee767
Andrew Hancox <andrewdchancox@googlemail.com>
2020-05-22 15:34:34 +0100

    mod_coursera: Error handling using one time code

--- mod/coursera/version.php
+++ mod/coursera/version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_coursera';
-$plugin->version = 2018101116;
+$plugin->version = 2018101117;
 $plugin->release = 'v0.0';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
